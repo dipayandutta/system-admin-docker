525f73d564a4c6b921cd2beda8348f613654c46a
Dipayan <inbox.dipayan@gmail.com>
2024-07-31 21:54:55 +0530

    owncloud configuration

---
+++ owncloud/ubuntu/config.php
@@ -0,0 +1,39 @@
+<?php
+$CONFIG = array (
+  'instanceid' => 'ocb3lplra3z4',
+  'passwordsalt' => 'HdoCOzwuMmpbqpik+NXkurRAkzdlGD',
+  'secret' => '1v/VjxbSgrsNfW/8I2CR6V4UCZvXefr7Ap3AI9Z9jeKIUWtH',
+  'trusted_domains' => 
+  array (
+    0 => 'localhost',
+  ),
+  'datadirectory' => '/var/www/html/owncloud/data',
+  'overwrite.cli.url' => 'http://localhost/owncloud',
+  'dbtype' => 'mysql',
+  'version' => '10.14.0.3',
+  'dbname' => 'owncloud',
+  'dbconnectionstring' => '',
+  'dbhost' => '192.168.25.25:3306',
+  'dbtableprefix' => 'oc_',
+  'mysql.utf8mb4' => true,
+  'dbuser' => 'owncloud',
+  'dbpassword' => 'owncloud',
+  'allow_user_to_change_mail_address' => '',
+  'logtimezone' => 'UTC',
+  'apps_paths' => 
+  array (
+    0 => 
+    array (
+      'path' => '/var/www/html/owncloud/apps',
+      'url' => '/apps',
+      'writable' => false,
+    ),
+    1 => 
+    array (
+      'path' => '/var/www/html/owncloud/apps-external',
+      'url' => '/apps-external',
+      'writable' => true,
+    ),
+  ),
+  'installed' => true,
+);
