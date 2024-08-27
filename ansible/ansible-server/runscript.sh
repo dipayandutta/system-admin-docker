13932766e12805888b4ee4491919452802ef99a3
dipayandutta <inbox.dipayan@gmail.com>
2024-08-27 06:13:11 +0530

    Ansible container Environment

---
+++ ansible/ansible-server/runscript.sh
@@ -0,0 +1,1 @@
+docker container run -dit -e TIMEZONE=Asia/kolkata -e ROOT_PASSWORD=dipayan --network ansiblenetwork --ip 192.168.60.100 --name ansible-server dockercommit/dipayandutta:ansible-server-rocky
